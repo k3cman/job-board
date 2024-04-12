fc797658000ada64e8a34f52e5ce7ffe73de5d7d
k3cman <kecman.dev@gmail.com>
2024-04-13 01:46:10 +0200

    clean up modules

--- src/app/jobs/feature/jobs.module.ts
+++ src/app/jobs/feature/jobs.module.ts
@@ -1,38 +1,17 @@
 import { NgModule } from '@angular/core';
 import { JobsRoutingModule } from './jobs-routing.module';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
-import {
-  MatCell,
-  MatCellDef,
-  MatColumnDef,
-  MatHeaderCell,
-  MatHeaderCellDef,
-  MatHeaderRow,
-  MatHeaderRowDef,
-  MatRow,
-  MatRowDef,
-  MatTable,
-} from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
 import { MatIcon } from '@angular/material/icon';
-import {
-  MatButton,
-  MatIconButton,
-  MatMiniFabButton,
-} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { EditJobDialogComponent } from './edit-job/edit-job.dialog';
 import { JobFormComponent } from '../ui/job-form/job-form.component';
 import { CreateJobDialogComponent } from './create-job/create-job.dialog';
 import { JobSkillsComponent } from '@shared/ui/job-skills/job-skills.component';
 import { SidebarDialogWrapperComponent } from '@shared/ui/sidebar-dialog-wrapper/sidebar-dialog-wrapper.component';
-import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
+import { MatMenuModule } from '@angular/material/menu';
 import { FilterJobsComponent } from './filter-jobs/filter-jobs.component';
-import {
-  MatChip,
-  MatChipGrid,
-  MatChipInput,
-  MatChipRow,
-  MatChipSet,
-} from '@angular/material/chips';
+import { MatChipsModule } from '@angular/material/chips';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -53,38 +32,21 @@ import { ViewJobsComponent } from './view-jobs/view-jobs.component';
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     JobsRoutingModule,
-    MatCell,
-    MatCellDef,
-    MatColumnDef,
-    MatHeaderCell,
-    MatHeaderRow,
-    MatHeaderRowDef,
+    MatTableModule,
     MatIcon,
-    MatIconButton,
-    MatMiniFabButton,
-    MatRow,
-    MatRowDef,
-    MatTable,
+    MatButtonModule,
     JobFormComponent,
-    MatHeaderCellDef,
     JobSkillsComponent,
     SidebarDialogWrapperComponent,
-    MatMenu,
-    MatMenuItem,
-    MatMenuTrigger,
-    MatButton,
-    MatChipGrid,
-    MatChipInput,
-    MatChipRow,
+    MatMenuModule,
+    MatChipsModule,
     MatFormField,
     MatInput,
     MatLabel,
-    ReactiveFormsModule,
     MatSelect,
     MatOption,
-    MatChip,
-    MatChipSet,
     PageHeadComponent,
     JobsTableComponent,
     FilterBarComponent,
